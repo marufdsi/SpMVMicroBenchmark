185b78f60d041667523ffa653134c8a4756f5a29
maruf <maruf.csea1@gmail.com>
2021-03-23 21:38:43 -0400

    codes are updated

--- src/fma_on_skylake.cpp
+++ src/fma_on_skylake.cpp
@@ -13,7 +13,7 @@
 
 const int n_trials = 1000000000; // Enough to keep cores busy for a while and observe a steady state
 const int flops_per_calc = 2; // Multiply + add = 2 instructions
-const int n_chained_fmas = 34; // Must be tuned for architectures here and in blocks (R) and in (E)
+const int n_chained_fmas = 33; // Must be tuned for architectures here and in blocks (R) and in (E)
 
 int main(int argc, char *argv[]) {
     int procs = 1;
@@ -65,7 +65,7 @@ int main(int argc, char *argv[]) {
         register double *fa31 = fa + 30 * VECTOR_WIDTH;
         register double *fa32 = fa + 31 * VECTOR_WIDTH;
         register double *fa33 = fa + 32 * VECTOR_WIDTH;
-        register double *fa34 = fa + 33 * VECTOR_WIDTH;
+//        register double *fa34 = fa + 33 * VECTOR_WIDTH;
 //        register double *fa35 = fa + 34 * VECTOR_WIDTH;
 
         int i, j;
@@ -106,7 +106,7 @@ int main(int argc, char *argv[]) {
                     fa31[j] = fa31[j] * fb[j] + fc[j];
                     fa32[j] = fa32[j] * fb[j] + fc[j];
                     fa33[j] = fa33[j] * fb[j] + fc[j];
-                    fa34[j] = fa34[j] * fb[j] + fc[j];
+//                    fa34[j] = fa34[j] * fb[j] + fc[j];
 //                    fa35[j] = fa35[j] * fb[j] + fc[j];
                 }
         fa[0:VECTOR_WIDTH * n_chained_fmas] *= 2.0; // Prevent dead code elimination
