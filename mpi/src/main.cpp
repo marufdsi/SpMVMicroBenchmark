77a25c205a23b2263d3a6c02cb19779b6d2375a5
maruf <maruf.csea1@gmail.com>
2021-03-26 11:55:43 -0400

    Script updated

--- mpi/src/main.cpp
+++ mpi/src/main.cpp
@@ -2,7 +2,8 @@
 // Created by maruf on 3/26/21.
 //
 #include <iostream>
-
+#include "fma_on_skylake.hpp"
+#include "utill.hpp"
 int main(int argc, char *argv[]){
     _fma_(argc, argv);
     return 0;
