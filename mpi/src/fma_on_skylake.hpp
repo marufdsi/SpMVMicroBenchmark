6fb5b4b1e81bf5e4c1b0c7c77d7f8f7e252af944
marufdsi <marufhossain.dsi@gmail.com>
2021-03-27 19:00:44 -0400

    Make file updated for mpi

--- mpi/src/fma_on_skylake.hpp
+++ mpi/src/fma_on_skylake.hpp
@@ -7,8 +7,17 @@
 
 #include "utill.hpp"
 
-class fma_on_skylake : public utill{
+#define VAL(X) (X)
+#ifndef N_CHAINED_FMAS
+N_CHAINED_FMAS = 8
+#endif
+
+class fma_on_skylake {
 public:
+    const int n_trials = 1000000000; // Enough to keep cores busy for a while and observe a steady state
+    const int flops_per_calc = 2; // Multiply + add = 2 instructions
+    const int n_chained_fmas = VAL(N_CHAINED_FMAS); // Must be tuned for architectures here and in blocks (R) and in (E)
+
     int _fma_(int argc, char *argv[]);
 };
 
--- mpi/src/utill.hpp
+++ mpi/src/utill.hpp
@@ -5,17 +5,9 @@
 #ifndef SPMVMICROBENCHMARK_UTILL_H
 #define SPMVMICROBENCHMARK_UTILL_H
 
-#define VAL(X) (X)
-#ifndef N_CHAINED_FMAS
-N_CHAINED_FMAS = 8
-#endif
+#define MASTER 0
 class utill {
 
-public:
-    const int n_trials = 1000000000; // Enough to keep cores busy for a while and observe a steady state
-    const int flops_per_calc = 2; // Multiply + add = 2 instructions
-    const int n_chained_fmas = VAL(N_CHAINED_FMAS); // Must be tuned for architectures here and in blocks (R) and in (E)
-    const int MASTER = 0;
 };
 
 
