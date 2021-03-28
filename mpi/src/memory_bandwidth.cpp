fcddc20222e063189c5a8cf5b739662f629e021d
marufdsi <marufhossain.dsi@gmail.com>
2021-03-28 05:39:41 -0400

    Make file updated for mpi

--- mpi/src/memory_bandwidth.cpp
+++ mpi/src/memory_bandwidth.cpp
@@ -7,7 +7,9 @@
 #include <omp.h>
 
 void memory_bandwidth::test_memory_bandwidth(int argc, char* argv[], int argi) {
-
+    int procs = 1;
+    if(argc>argi)
+        procs = atoi(argv[argi]);
     /* Initialize MPI */
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &nRanks);
@@ -21,16 +23,39 @@ void memory_bandwidth::test_memory_bandwidth(int argc, char* argv[], int argi) {
         dram_array_aligned[i] = i%20;
     }
     MPI_Barrier(MPI_COMM_WORLD);
-    std::vector<double> seq_mem_bandwidth = sequential_read(dram_array_aligned, mem_size);
+    std::vector<std::pair<double, double> > seq_mem_bandwidth = sequential_read(dram_array_aligned, mem_size);
     MPI_Barrier(MPI_COMM_WORLD);
+    if(rank == MASTER){
+        std::ofstream resultCSV;
+        std::string folderName = "Results/";
+        std::string fileName = procs == 2 ? "SEQ_READ_ON_CASCADE_LAKE.csv" : "SEQ_READ_ON_SKYLAKE.csv";
+        if (mkdir(folderName.c_str(), 0777) == -1)
+            std::cout << "Directory " << folderName << " is already exist" << std::endl;
+        else
+            std::cout << "Directory " << folderName << " created" << std::endl;
+        std::ifstream infile(folderName + fileName);
+        resultCSV.open(folderName + fileName, std::ios_base::out | std::ios_base::app | std::ios_base::ate);
+
+        if (!infile.good()) {
+            resultCSV
+                    << "Size,AvgTime,AvgBandwidth,Processes"
+                    << std::endl;
+        }
+        infile.close();
+        for(auto p : seq_mem_bandwidth) {
+            resultCSV << mem_size << "," << p.first << "," << p.second << "," << nRanks << std::endl;
+        }
+        resultCSV.close();
+    }
     delete dram_array;
     MPI_Finalize();
 }
 
-std::vector<double> memory_bandwidth::sequential_read(char *arr, size_t size) {
+std::vector<std::pair<double, double> > memory_bandwidth::sequential_read(char *arr, size_t size) {
     std::vector<double> bandwidth;
     __m256i s;
-    for (uint32_t iter = 1; iter <= iterations; iter++) {
+    double total_time = 0, total_bandwidth = 0, avg_time = 0, avg_bandwidth = 0;
+    for (uint32_t iter = 1; iter <= iterations+skip; iter++) {
         const double t0 = MPI_Wtime();
         s = _mm256_setzero_si256();
         __m256i* test_mem = reinterpret_cast<__m256i*>(arr);
@@ -40,11 +65,23 @@ std::vector<double> memory_bandwidth::sequential_read(char *arr, size_t size) {
             s += _mm256_load_si256(test_mem + i);
         }
         const double t1 = MPI_Wtime();
-        double time_elapsed = t1 - t0;
-        bandwidth.push_back((double)size / time_elapsed);
-        if(rank == MASTER)
-            std::cout<< iter << " " << time_elapsed << " " << bandwidth.back() << std::endl;
+        double time_elapsed = (t1 - t0);
+        if(iter>=skip) {
+            total_time += time_elapsed;
+            total_bandwidth += (double) size / time_elapsed;
+        }
     }
+    total_time /= iterations;
+    total_bandwidth /= iterations;
+    MPI_Reduce(&total_time, &avg_time, 1, MPI_DOUBLE, MPI_SUM, MASTER, MPI_COMM_WORLD);
+    avg_time /= nRanks;
+
+    MPI_Reduce(&total_bandwidth, &avg_bandwidth, 1, MPI_DOUBLE, MPI_SUM, MASTER, MPI_COMM_WORLD);
+    avg_bandwidth /= nRanks;
+    bandwidth.push_back({avg_time, avg_bandwidth});
+    if(rank == MASTER)
+        std::cout<< iter << " " << avg_time << " " << avg_bandwidth << std::endl;
+
     unsigned long long sum = _mm256_extract_epi64(s, 0);
     std::cout<< "IGNORE( " << sum << " )" << std::endl;
     return bandwidth;
--- mpi/src/memory_bandwidth.hpp
+++ mpi/src/memory_bandwidth.hpp
@@ -18,22 +18,25 @@ private:
     int rank;
 public:
     uint32_t iterations;
+    uint32_t skip;
     uint32_t mem_size;
-    memory_bandwidth(size_t mem_size, uint32_t iterations):mem_size(mem_size), iterations(iterations){}
+    memory_bandwidth(size_t mem_size, uint32_t iterations, uint32_t skip=5):mem_size(mem_size), iterations(iterations), skip(skip){}
     memory_bandwidth(uint32_t iterations):iterations(iterations){
         mem_size = 1000000 ;
+        skip = 5;
     }
     memory_bandwidth(){
         mem_size = 1000000 ;
         iterations = 10 ;
+        skip = 5;
     }
 
     void test_memory_bandwidth(int argc, char* argv[], int argi);
 
-    std::vector<double> sequential_read(char* arr, size_t size);
-    std::vector<double> random_read(char* arr, size_t size);
-    std::vector<double> sequential_write(char* arr, size_t size);
-    std::vector<double> random_write(char* arr, size_t size);
+    std::vector<std::pair<double, double> > sequential_read(char* arr, size_t size);
+    std::vector<std::pair<double, double> > random_read(char* arr, size_t size);
+    std::vector<std::pair<double, double> > sequential_write(char* arr, size_t size);
+    std::vector<std::pair<double, double> > random_write(char* arr, size_t size);
 
 };
 
