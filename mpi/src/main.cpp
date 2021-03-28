8bd9080e408cd1f5c646626da623c7ce0eb79055
marufdsi <marufhossain.dsi@gmail.com>
2021-03-28 01:16:36 -0400

    Make file updated for mpi

--- mpi/src/main.cpp
+++ mpi/src/main.cpp
@@ -16,7 +16,15 @@ int main(int argc, char *argv[]){
         fma_obj._fma_(argc, argv, argi);
     } else if(atoi(argv[argi]) == my_util.MEMORY_BANDWIDTH) {
         argi++;
-        memory_bandwidth memoryBandwidth(10);
+        size_t mem_size = 1000000;
+        uint32_t iterations=10;
+        if(argc > argi){
+            mem_size = atoi(argv[argi++])
+        }
+        if(argc > argi){
+            iterations = atoi(argv[argi++])
+        }
+        memory_bandwidth memoryBandwidth(mem_size, iterations);
         memoryBandwidth.test_memory_bandwidth(argc, argv, argi);
     }
     return 0;
