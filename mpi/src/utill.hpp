6aba52f57e60eeb45d692f3a293022fdf7c8805c
marufdsi <marufhossain.dsi@gmail.com>
2021-03-28 00:40:39 -0400

    Make file updated for mpi

--- mpi/src/utill.hpp
+++ mpi/src/utill.hpp
@@ -9,11 +9,11 @@
 
 #define MASTER 0
 class utill {
-    std::unordered_map<std::string, int> ALGORITHMS{
-        std::pair{"FMA", 1},
-        std::pair{"SEQUENTIAL_READ", 2},
-        std::pair{"RANDOM_READ", 3}
-    };
+    std::unordered_map<std::string, int> ALGORITHMS({
+        {"FMA", 1},
+        {"SEQUENTIAL_READ", 2},
+        {"RANDOM_READ", 3}
+    });
     std::enum ALGO_NAMES{
         FMA,
         SEQUENTIAL_READ,
